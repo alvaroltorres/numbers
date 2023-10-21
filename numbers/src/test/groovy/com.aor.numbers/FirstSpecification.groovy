5cf3f113af8fd92a115137d0e14a0f9daa02e791
unknown <up202208954@up.pt>
2023-10-21 12:24:12 +0100

    finished step 11.5

--- numbers/src/test/groovy/com.aor.numbers/FirstSpecification.groovy
+++ numbers/src/test/groovy/com.aor.numbers/FirstSpecification.groovy
@@ -28,12 +28,22 @@ class FirstSpecification extends Specification {
     }
 
     def "Should get an index out of bounds when removing a non-existent item"() {
-    given:
-        def list = [1, 2, 3, 4]
-    when:
-        list.remove(20)
-    then:
-        thrown(IndexOutOfBoundsException)
-        list.size() == 4
+        given:
+            def list = [1, 2, 3, 4]
+        when:
+            list.remove(20)
+        then:
+            thrown(IndexOutOfBoundsException)
+            list.size() == 4
+    }
+
+    def "numbers to the power of two"(int a, int b, int c) {
+        expect:
+            Math.pow(a, b) == c
+        where:
+            a | b | c
+            1 | 2 | 1
+            2 | 2 | 4
+            3 | 2 | 9
     }
 }
